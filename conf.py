23e3c3f7bceda70865460c2094f2de9da8040785
invisaligndoctorlogin <109352093+invisaligndoctorlogin@users.noreply.github.com>
2025-07-10 11:32:37 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'DYMO Label Software'
 
 
 # The full version, including alpha/beta/rc tags
